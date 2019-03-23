a47821f451a385db10450a718c697cf1469aaf7f
Giancarlo Rocha <giancarloiff@gmail.com>
2019-03-22 22:14:16 -0300

    initial commit

---
+++ Trabalhos P2/trabalho 1 aula/trabalho1aula.js
@@ -0,0 +1,33 @@
+function esconder1() {
+  var enlace_1 = document.getElementById('conteudo_1');
+  var troca1 = document.getElementById('enlace_1');
+  enlace_1.style.display = enlace_1.style.display === 'none' ? '' : 'none';
+   if (troca1.innerHTML=="Mostrar Conteúdo"){
+  	troca1.innerHTML="Ocultar Conteúdo"
+  } else {
+  	troca1.innerHTML="Mostrar Conteúdo"
+  }
+}
+
+function esconder2() {
+    var enlace_2 = document.getElementById('conteudo_2');
+    var troca2 = document.getElementById('enlace_2');
+    enlace_2.style.display = enlace_2.style.display === 'none' ? '' : 'none';
+
+     if (troca2.innerHTML=="Mostrar Conteúdo"){
+  	troca2.innerHTML="Ocultar Conteúdo"
+  } else {
+  	troca2.innerHTML="Mostrar Conteúdo"
+  }
+}
+
+function esconder3() {
+    var enlace_3 = document.getElementById('conteudo_3');
+    var troca3 = document.getElementById('enlace_3');
+    enlace_3.style.display = enlace_3.style.display === 'none' ? '' : 'none';
+     if (troca3.innerHTML=="Mostrar Conteúdo"){
+  	troca3.innerHTML="Ocultar Conteúdo"
+  } else {
+  	troca3.innerHTML="Mostrar Conteúdo"
+  }
+}
